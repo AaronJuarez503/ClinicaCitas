e4ce6b1cd2006a70b7aa82817162c4d76cd4680f
Jazmin Eugenia Lue Duran <ACER@LAPTOP-FRMPB979>
2025-08-20 22:47:54 -0600

    service de medico y paciente

--- src/main/java/org/esfe/servicios/implementaciones/MedicoServiceImpl.java
+++ src/main/java/org/esfe/servicios/implementaciones/MedicoServiceImpl.java
@@ -11,6 +11,8 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+
+
 @Service
 public class MedicoServiceImpl implements IMedicoService {
 
