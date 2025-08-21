92ad27aeaede0af797eca41af854a90783dcbcdf
Aaron Raymundo Juarez <je25001@esfe.agape.edu.sv>
2025-08-21 10:57:22 -0600

    creacion del generador de pdf

--- src/main/java/org/esfe/controladores/ClinicaController.java
+++ src/main/java/org/esfe/controladores/ClinicaController.java
@@ -1,27 +1,41 @@
 package org.esfe.controladores;
 
+import java.io.IOException;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 import org.esfe.modelos.Clinica;
+import org.esfe.servicios.PdfGeneratorService;
 import org.esfe.servicios.interfaces.IClinicaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.List;
-import java.util.Optional;
-import java.util.stream.Collectors;
-import java.util.stream.IntStream;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 
 @Controller
 @RequestMapping("/clinica")
 public class ClinicaController {
     @Autowired
     private IClinicaService clinicaService;
+
+    @Autowired
+    private PdfGeneratorService pdfGeneratorService;
 
     @GetMapping
     public String index(Model model, @RequestParam("page") Optional<Integer> page, @RequestParam("size") Optional<Integer> size) {
@@ -90,6 +104,27 @@ public String crear(Clinica clinica) {
         clinicaService.eliminarPorId(clinica.getId());
         attributes.addFlashAttribute("msg", "clinica eliminada correctamente");
         return "redirect:/clinica";
+    }
+
+    @GetMapping("/reporte/{visualizacion}")
+    public ResponseEntity<byte[]> ReporteGeneral(@PathVariable("visualizacion") String visualizacion) {
+
+        try {
+            List<Clinica> grupos = clinicaService.obtenerTodos();
+
+            // Genera el PDF. Si hay un error aquí, la excepción será capturada.
+            byte[] pdfBytes = pdfGeneratorService.generatePdfFromHtml("reporte/reporte", "clinicas", grupos);
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_PDF);           
+            // inline= vista previa, attachment=descarga el archivo
+           headers.add("Content-Disposition", visualizacion+"; filename=reporte_general.pdf");
+
+            return new ResponseEntity<>(pdfBytes, headers, HttpStatus.OK);
+
+        } catch (IOException e) {
+            return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 
 }
